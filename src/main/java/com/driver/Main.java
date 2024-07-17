ee8eb647cf8a6dc518ecbe41ac061c9c7afcf91c
PrashanthPremchand <prashanthpremchand@gmail.com>
2024-07-17 21:15:38 +0530

    changed the class product location

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,21 @@
 package com.driver;
 
 public class Main {
-
-
+    static class  Product{
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
         int p1 = p.product(10, 20);
         int p2 = p.product(10, 20, 30);
         double p3 = p.product(10d, 20d);
-    }
-}
-class  Product{
-    public int product(int x, int y) {
-        return x * y;
-    }
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-    public double product(double x, double y) {
-        return x * y;
     }
 }
